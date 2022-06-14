c16bd7912abf08915e7bff395e8e3fc447e7f582
ANINDA <anindadutta1994@gmail.com>
2022-06-14 20:47:15 +0530

    new

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x7FA480d15e30e0adF8f59b47631ACad39d573214";
+// const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "address-of-the-nft-contract";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
